cfbd61955bab23af63f2161b6b6f8bdcea9e3f56
Jonatas Eleoterio <jonatas.eleoterio@agencian1.com.br>
2022-02-01 03:41:18 -0300

    chore: add prettier

---
+++ backend/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: 'standard-with-typescript',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
+  },
+  rules: {
+    'space-before-function-paren': 'error'
+  },
+  'space-before-function-paren': 'off',
+  '@typescript-eslint/space-before-function-paren': ['error']
+}
