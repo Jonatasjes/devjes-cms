9ecc4f67aac2ace1e9523923fe2518e5164f8b7f
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-01 23:19:46 -0300

    test: ensure SignUpController return an error if no last_name is provided

--- backend/src/presentation/controllers/signup.spec.ts
+++ backend/src/presentation/controllers/signup.spec.ts
@@ -16,4 +16,20 @@ describe('Signup Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: first_name'))
   })
+
+  test('Should return 400 if on last name is provided', () => {
+    const sut = new SignupController()
+    const httpRequest = {
+      body: {
+        first_name: 'any_first_name',
+        username: 'any_username',
+        password: 'any_password',
+        password_confirmation: 'any_password_confirmation'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: last_name'))
+  })
 })
