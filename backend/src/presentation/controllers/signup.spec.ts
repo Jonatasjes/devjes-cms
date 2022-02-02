afab96912dd2caa865af4dc0b9f95303e7629303
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-02 01:20:25 -0300

    refactor: move sut creation to a factory helper method

--- backend/src/presentation/controllers/signup.spec.ts
+++ backend/src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,13 @@
 import { MissingParameterError } from '../errors/missing-param-error'
-import { SignupController } from './signup'
+import { SignUpController } from './signup'
+
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
 
 describe('Signup Controller', () => {
   test('Should return 400 if on first name is provided', () => {
-    const sut = new SignupController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         last_name: 'any_last_name',
@@ -20,7 +24,7 @@ describe('Signup Controller', () => {
   })
 
   test('Should return 400 if on last name is provided', () => {
-    const sut = new SignupController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         first_name: 'any_first_name',
@@ -37,7 +41,7 @@ describe('Signup Controller', () => {
   })
 
   test('Should return 400 if on username is provided', () => {
-    const sut = new SignupController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         first_name: 'any_first_name',
@@ -54,7 +58,7 @@ describe('Signup Controller', () => {
   })
 
   test('Should return 400 if on email is provided', () => {
-    const sut = new SignupController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         first_name: 'any_first_name',
@@ -71,7 +75,7 @@ describe('Signup Controller', () => {
   })
 
   test('Should return 400 if on password is provided', () => {
-    const sut = new SignupController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         first_name: 'any_first_name',
@@ -88,7 +92,7 @@ describe('Signup Controller', () => {
   })
 
   test('Should return 400 if on password confirmation is provided', () => {
-    const sut = new SignupController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         first_name: 'any_first_name',
