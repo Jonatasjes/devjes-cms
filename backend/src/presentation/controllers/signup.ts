47992da3c164cbc1ac9dd07686037cec0d43650f
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-01 23:19:29 -0300

    feat: ensure SignUpController return an error if no last_name is provided

--- backend/src/presentation/controllers/signup.ts
+++ backend/src/presentation/controllers/signup.ts
@@ -1,8 +1,17 @@
 export class SignupController {
   handle(httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: first_name')
+    if (!httpRequest.body.first_name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: first_name')
+      }
+    }
+
+    if (!httpRequest.body.last_name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: last_name')
+      }
     }
   }
 }
