7c0a54d6eacb46228d412e29899a3fad4888254b
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-02 00:51:48 -0300

    refactor: mode duplicated code to a helper method

--- backend/src/presentation/controllers/signup.ts
+++ backend/src/presentation/controllers/signup.ts
@@ -1,39 +1,25 @@
 import { MissingParameterError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignupController {
   handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.first_name) {
-      return {
-        statusCode: 400,
-        body: new MissingParameterError('first_name')
-      }
+      return badRequest(new MissingParameterError('first_name'))
     }
 
     if (!httpRequest.body.last_name) {
-      return {
-        statusCode: 400,
-        body: new MissingParameterError('last_name')
-      }
+      return badRequest(new MissingParameterError('last_name'))
     }
 
     if (!httpRequest.body.username) {
-      return {
-        statusCode: 400,
-        body: new MissingParameterError('username')
-      }
+      return badRequest(new MissingParameterError('username'))
     }
 
     if (!httpRequest.body.email) {
-      return {
-        statusCode: 400,
-        body: new MissingParameterError('email')
-      }
+      return badRequest(new MissingParameterError('email'))
     }
 
-    return {
-      statusCode: 400,
-      body: new MissingParameterError('Missing all param')
-    }
+    return badRequest(new MissingParameterError('Missing all param'))
   }
 }
