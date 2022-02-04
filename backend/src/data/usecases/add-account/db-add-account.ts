7adfad20e9706d2c80bf1c9b9f3ff9ea5625e365
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-04 00:07:15 -0300

    feat: ensure DbAddAccount returns an account on success

--- backend/src/data/usecases/add-account/db-add-account.ts
+++ backend/src/data/usecases/add-account/db-add-account.ts
@@ -19,18 +19,9 @@ export class DbAddAccount implements AddAccount {
   }
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
-    await this.addAccountRepository.add(
+    const account = await this.addAccountRepository.add(
       Object.assign({}, accountData, { password: hashedPassword })
     )
-    return new Promise(resolve =>
-      resolve({
-        id: 'null',
-        first_name: 'null',
-        last_name: 'null',
-        username: 'null',
-        email: 'null',
-        password: 'null'
-      })
-    )
+    return account
   }
 }
