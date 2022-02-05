c595f095f7a364f831f1b9d56b73b16b7187fc83
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-05 18:34:28 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- backend/src/main/decorators/log.spec.ts
+++ backend/src/main/decorators/log.spec.ts
@@ -56,4 +56,30 @@ describe('LogController Decorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut, controllerStub } = makeSut()
+    const httpRequest = {
+      body: {
+        first_name: 'any_first_name',
+        last_name: 'any_last_name',
+        username: 'any_username',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        first_name: 'Jonatas',
+        last_name: 'Silva',
+        username: 'jonatassilva',
+        email: 'jonatas@mail.com',
+        password: 'jonatas123',
+        passwordConfirmation: 'jonatas123'
+      }
+    })
+  })
 })
