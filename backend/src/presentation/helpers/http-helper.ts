54404c6c0e9edff4d7eda620c80c9f688b0681d2
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-05 18:45:50 -0300

    refactor: make server error return the error stack

--- backend/src/presentation/helpers/http-helper.ts
+++ backend/src/presentation/helpers/http-helper.ts
@@ -5,9 +5,9 @@ export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error.message
 })
-export const serverError = (): HttpResponse => ({
+export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
-  body: new ServerError()
+  body: new ServerError(error.stack as string)
 })
 
 export const ok = (data: any): HttpResponse => ({
