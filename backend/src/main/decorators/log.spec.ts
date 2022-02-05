5bf87d651555f3b1a243c5d3520b6e1c2276aa39
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-05 13:38:45 -0300

    refactor: move sut creation to a factory method

--- backend/src/main/decorators/log.spec.ts
+++ backend/src/main/decorators/log.spec.ts
@@ -5,27 +5,44 @@ import {
 } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse = {
+        statusCode: 200,
+        body: {
+          first_name: 'Jonatas',
+          last_name: 'Silva',
+          username: 'jonatassilva',
+          email: 'jonatas@mail.com',
+          password: 'jonatas123',
+          passwordConfirmation: 'jonatas123'
+        }
+      }
+      return new Promise(resolve => resolve(httpResponse))
+    }
+  }
+  return new ControllerStub()
+}
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
 describe('LogController Decorator', () => {
   test('Should call controller handle', async () => {
-    class ControllerStub implements Controller {
-      async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse = {
-          statusCode: 200,
-          body: {
-            first_name: 'Jonatas',
-            last_name: 'Silva',
-            username: 'jonatassilva',
-            email: 'jonatas@mail.com',
-            password: 'jonatas123',
-            passwordConfirmation: 'jonatas123'
-          }
-        }
-        return new Promise(resolve => resolve(httpResponse))
-      }
-    }
-    const controllerStub = new ControllerStub()
+    const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const sut = new LogControllerDecorator(controllerStub)
     const httpRequest = {
       body: {
         first_name: 'any_first_name',
