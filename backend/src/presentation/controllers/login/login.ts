6fe14faf19ddafac4e85bdb4a5c8b58bbf05ef2d
Jonatas Eleoterio Silva <jonatas.eleoterio@outlook.com>
2022-02-06 02:21:49 -0300

    feat: ensure LoginController returns 500 if EmailValidator throws

--- backend/src/presentation/controllers/login/login.ts
+++ backend/src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, ok } from '../../helpers/http-helper'
+import { badRequest, ok, serverError } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { EmailValidator } from '../../protocols/email-validator'
 
@@ -9,27 +9,30 @@ export class LoginController implements Controller {
     this.emailValidator = emailValidator
   }
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
+    try {
+      const { email, password } = httpRequest.body
+      if (!email) {
+        return new Promise(resolve =>
+          resolve(badRequest(new MissingParamError('email')))
+        )
+      }
+      if (!password) {
+        return new Promise(resolve =>
+          resolve(badRequest(new MissingParamError('password')))
+        )
+      }
 
-    if (!email) {
-      return new Promise(resolve =>
-        resolve(badRequest(new MissingParamError('email')))
-      )
+      const isValid = this.emailValidator.isValid(email)
+
+      if (!isValid) {
+        return new Promise(resolve =>
+          resolve(badRequest(new InvalidParamError('email')))
+        )
+      }
+
+      return new Promise(resolve => resolve(ok({})))
+    } catch (error) {
+      return serverError(error)
     }
-    if (!password) {
-      return new Promise(resolve =>
-        resolve(badRequest(new MissingParamError('password')))
-      )
-    }
-
-    const isValid = this.emailValidator.isValid(email)
-
-    if (!isValid) {
-      return new Promise(resolve =>
-        resolve(badRequest(new InvalidParamError('email')))
-      )
-    }
-
-    return new Promise(resolve => resolve(ok({})))
   }
 }
